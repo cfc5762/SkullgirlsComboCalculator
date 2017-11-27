d865ef631324e9ef3d71eb4259faf0883892e179
Chris Commisso <31491130+cfc5762@users.noreply.github.com>
2017-11-27 15:06:46 -0500

    The project file

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(992, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(992, 31);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 112);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(209, 346);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(697, 264);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 800);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Silktails Combo calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int drama = 0;
+        double totalDamage = 0;
+        double scaling = 1.0;
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Text = "Welcome to Silktail's damage calculator Combos should be inputted in the following manner \"initial scaling value,hit,hit...\"  hits should be formatted as such.  if the hit adds drama input \"damage: drama\" otherwise just use \"damage\"  the initial scaling value is dependant on team sizes and counter hit \n\nif there are scaling values I do not take into account (such as dhc or tag) after damage and drama you may place a ^ followed by a decimal value to auto set the scaling value, a * followed by a decimal value to multiply a decimal by the scalar and a $ followed by a decimal value to set a minimum scaling for this move...\nhave fun";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Text = totalDamage.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drama = 0;
+            int tempdrama = 0;
+            totalDamage = 0;
+            double damage = 0;
+            String[] combo = textBox1.Text.Split(',');
+            double.TryParse(combo[0], out scaling);
+            for (int i = 1; i < combo.Length; i++)
+            {
+                if (i > 3)//set the scaling for this hit
+                {
+                    if (scaling > .2)
+                        scaling *= .875;
+
+                }
+                double.TryParse(combo[i].Split(':')[0], out damage);
+                if (damage >= 1000 && scaling < .275)//calculate hits damage
+                {
+                    damage *= .275;
+                }
+                else
+                {
+                    damage *= scaling;
+                }
+                if (drama >= 240)
+                {
+                    damage *= .5;
+                }
+                if (combo[i].Split(':').Length > 1)//is there drama
+                {
+                    int.TryParse(combo[i].Split(':')[1], out tempdrama);
+                    drama += tempdrama;
+                    tempdrama = 0;//add to dra,a
+                }
+                if (combo[i].Split('^').Length > 1)
+                {
+                    double.TryParse(combo[i].Split('^')[1], out scaling);
+                }
+                else if (combo[i].Split('*').Length > 1)
+                {
+                    double scalar = 1;
+                    double.TryParse(combo[i].Split('*')[1], out scalar);
+                    scaling *= scalar;
+                }
+                else if (combo[i].Split('$').Length > 1)
+                {
+                    double scalar = 1;
+                    double.TryParse(combo[i].Split('$')[1], out scalar);
+                    if (scaling < scalar)
+                    {
+                        scaling = scalar;
+                    }
+                }
+                totalDamage += damage;//add the damage
+                textBox2.Text = "Damage = "+totalDamage.ToString()+" Drama = "+drama;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Welcome to silktails damage calculator Combos should be inputted in the following manner \"initial scaling value,hit,hit...\"  hits should be formatted as such.  if the hit adds drama input \"damage: drama\" otherwise just use \"damage\"  the initial scaling value is dependant on team sizes and counter hit \n\nif there are scaling values I do not take into account (such as dhc or tag) after damage and drama you may place a ^followed by a decimal value to auto set the scaling value or *followed by a decimal value to multiply a decimal by the scalar...\nhave fun";
+
+        }
+    }
+}
